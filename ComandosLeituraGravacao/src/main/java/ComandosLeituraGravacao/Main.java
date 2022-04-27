c4a495595b5705320752b3cacd882fe242ec4cdf
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-04-27 20:59:54 -0300

    entrada e saida de dados

--- ComandosLeituraGravacao/src/main/java/ComandosLeituraGravacao/Main.java
+++ ComandosLeituraGravacao/src/main/java/ComandosLeituraGravacao/Main.java
@@ -19,6 +19,17 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        Scanner leitor = new Scanner(System.in);
+        
+        int idade = leitor.nextInt();
+        float cotacaoDolar = leitor.nextFloat();
+        double cotacaoEuro = leitor.nextDouble();
+        String nome = leitor.nextLine();
+        String codigoRua = leitor.next();
+        
+        System.out.println("Texto que será exibido no console");
+        System.out.print("Texto que será exibido no console");
+        
         
         
     }
