88535be45261fac78e3ced6286a8c3b451645035
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-04-29 18:03:09 -0300

    aula de estrutura cndicional e operadores

--- EstruturaCondicionalSe/src/main/java/EstruturaCondicionalSe/Main.java
+++ EstruturaCondicionalSe/src/main/java/EstruturaCondicionalSe/Main.java
@@ -1,21 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package EstruturaCondicionalSe;
 
-/**
- *
- * @author Martarelli
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        int media =7;
+        
+        /*
+        OPERADORES RELACIONAIS
+        >   >=  <   <=  ==  !=
+        
+        OPERADORES LOGICOS
+        E-> &&  OU -> ||  NEGAÇÃO-> !
+        */
+        
+        if(media >=7){
+            if(media == 10){
+                //APROVADO COM NOTA MAXIMA
+            } else {
+                //APROVADO
+            }       
+        } else {
+            //REPROVADO           
+        }
+        
     }
     
 }
