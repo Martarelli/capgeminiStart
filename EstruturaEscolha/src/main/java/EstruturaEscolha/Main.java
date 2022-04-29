c3cc2b29863b37a5b78f3de0605d3c973c73d4cd
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-04-29 18:08:01 -0300

    Finalizada aula de switch case

--- EstruturaEscolha/src/main/java/EstruturaEscolha/Main.java
+++ EstruturaEscolha/src/main/java/EstruturaEscolha/Main.java
@@ -4,6 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int codigoProduto = 2;
+        
+        switch(codigoProduto){
+            
+            case 1:
+                
+                break;
+                
+            case 2:
+                
+                break;
+            
+            default:
+        }
     }
     
 }
