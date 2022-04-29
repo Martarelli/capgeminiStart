a53481e843187fa73b77b297a094aa911260cea7
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-04-29 18:53:37 -0300

    Aula de estrutura While finalizada

--- EstruturaWhile/src/main/java/EstruturaWhile/Main.java
+++ EstruturaWhile/src/main/java/EstruturaWhile/Main.java
@@ -1,9 +1,24 @@
 package EstruturaWhile;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+                        
+        int totalAlunos = 10;
+        Scanner leitorScanner = new Scanner(System.in);
+        while(totalAlunos > 0){
+            
+            String nomeAluno = leitorScanner.nextLine();
+            int idadeAluno = leitorScanner.nextInt();
+            
+            System.out.println("O nome do aluno é: " + nomeAluno + " e sua idade"
+                    + " é: " + idadeAluno);
+            
+            totalAlunos--; 
+            System.out.println(totalAlunos);
+        }
         
     }
     
