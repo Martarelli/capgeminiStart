c8a724c37dca82b23ac12862d04290cf8d4847c1
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-05-04 18:28:23 -0300

    aula de classe concluida

--- Classes/src/main/java/Classes/Main.java
+++ Classes/src/main/java/Classes/Main.java
@@ -1,9 +1,22 @@
 package Classes;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        
+        Pessoa objetoPessoa = new Pessoa();
+        
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("Digite o peso da pessoa: ");
+        objetoPessoa.peso = leitor.nextFloat();
+        
+        System.out.println("Digite a altura da pessoa: ");
+        objetoPessoa.altura = leitor.nextFloat();
+        
+        System.out.println("IMC = " + objetoPessoa.calcularIMC());
+        
     }
     
 }
--- Classes/src/main/java/Classes/Pessoa.java
+++ Classes/src/main/java/Classes/Pessoa.java
@@ -2,7 +2,13 @@ package Classes;
 
 public class Pessoa {
     
+    //Atributos
     float peso;
     float altura;
     
+    //Metodos
+    public float calcularIMC(){
+        float imc = peso / (altura * altura);
+        return imc;
+    }
 }
