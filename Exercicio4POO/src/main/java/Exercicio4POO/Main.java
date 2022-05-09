7dc43d17cbc4f6e5c0a7f7d8c2ef4a9848603272
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-05-09 18:44:50 -0300

    Exercicio finalizado

--- Exercicio4POO/src/main/java/Exercicio4POO/Main.java
+++ Exercicio4POO/src/main/java/Exercicio4POO/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Exercicio4POO;
 
 /**
@@ -11,11 +6,12 @@ package Exercicio4POO;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Invoice inv = new Invoice(1, "Mousepad", -10, 50.0f);
+        
+        System.out.println("Valor total = " + inv.getInvoiceAmount());
+        
     }
     
 }
