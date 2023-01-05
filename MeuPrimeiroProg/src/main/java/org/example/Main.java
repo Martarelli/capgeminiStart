eef4be2e991306815cd9e3e1e4525ed1f9d42fe3
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-01-04 22:55:06 -0300

    primeiro prog java essencial

---
+++ MeuPrimeiroProg/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        float numero = 0.5f;
+        numero = 130.533f;
+        System.out.println("Valor de número é: " + numero);
+    }
+}
