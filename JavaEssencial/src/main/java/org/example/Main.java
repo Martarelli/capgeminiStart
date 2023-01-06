ffa33ed899b45cd401f688d0f0c77792d04a3e80
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-01-05 21:09:03 -0300

    aula literais

---
+++ JavaEssencial/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int numero;
+        String cadeiaCaracteres;
+        char caracter;
+        boolean certoOuErrado;
+
+        numero = 10;
+        cadeiaCaracteres = "Curso java fundamentos";
+        caracter = 'S';
+        certoOuErrado = true;
+
+        System.out.println("O valor de número é: " + numero);
+        System.out.println("O valor de cadeia caracteres é: " + cadeiaCaracteres);
+        System.out.println("O valor de caracter é: " + caracter);
+        System.out.println("O valor de certo ou errado é: " + certoOuErrado);
+
+
+    }
+}
