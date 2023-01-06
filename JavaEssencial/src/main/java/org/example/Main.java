d7e896e420bbe28acffb5c926e4c673b869ac519
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-01-05 21:13:43 -0300

    operador aritmetico simples

--- JavaEssencial/src/main/java/org/example/Main.java
+++ JavaEssencial/src/main/java/org/example/Main.java
@@ -18,6 +18,11 @@ public class Main {
         System.out.println("O valor de caracter é: " + caracter);
         System.out.println("O valor de certo ou errado é: " + certoOuErrado);
 
+        int num1, num2, num3;
+        num1 = 10;
+        num2 = 20;
+        num3 = num1 + num2;
 
+        System.out.println(num3);
     }
 }
