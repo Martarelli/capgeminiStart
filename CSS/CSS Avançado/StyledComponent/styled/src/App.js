0c0acb8b83e7b0a1504e31a8029dc4dc5a9fd3fd
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-02-03 19:15:53 -0300

    adicionado o styled component ao projeto

--- CSS/CSS Avançado/StyledComponent/styled/src/App.js
+++ CSS/CSS Avançado/StyledComponent/styled/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Exercicio de Styled Components com React
         </p>
         <a
           className="App-link"
