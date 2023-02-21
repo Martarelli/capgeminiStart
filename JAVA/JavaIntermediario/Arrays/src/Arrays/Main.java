8136269adf6e1f6bee73e40dec0fc745437c4948
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-02-21 19:28:36 -0300

    utilizando array

--- JAVA/JavaIntermediario/Arrays/src/Arrays/Main.java
+++ JAVA/JavaIntermediario/Arrays/src/Arrays/Main.java
@@ -5,6 +5,16 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String[] paises = new String[4];
+		
+		paises[0] = "Brasil";
+		paises[1] = "Argentina";
+		paises[2] = "EUA";
+		paises[3] = "Japão";
+		
+		for (String string : paises) {
+			System.out.println(string);
+		}
 	}
 
 }
