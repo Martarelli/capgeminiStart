a0e6518594313d9e585712ca9e14b7dae0036ab0
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-02-21 20:07:59 -0300

    trabalhando com matrizes

--- JAVA/JavaIntermediario/Matrizes/src/Matrizes/Main.java
+++ JAVA/JavaIntermediario/Matrizes/src/Matrizes/Main.java
@@ -1,10 +1,44 @@
 package Matrizes;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String[] alunos = {"JOAO", "MARIA", "JOSÉ"};
+		
+		float [][] notas = new float[3][4];
+		
+		float somaNotas, mediaAluno;
+		String statusAluno;
+		
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Aluno: " + alunos[i]);
+			somaNotas = 0;
+			mediaAluno = 0;
+			for (int j = 0; j < 4; j++) {
+				notas[i][j] = Float.parseFloat(JOptionPane.showInputDialog("Digite a nota:" + (j+1)));
+				somaNotas += notas[i][j];
+			}
+			
+			mediaAluno = somaNotas / 4;
+			if (mediaAluno < 5.5) {
+				statusAluno = "REPROVADO";
+			} else if (mediaAluno >= 5.5 && mediaAluno <= 7.5){
+				statusAluno = "RECUPERAÇÃO";
+			} else {
+				statusAluno = "APROVADO";
+			}
+			
+			System.out.println("\n***********BOLETIM***********");
+			System.out.println("Aluno: " + alunos[i]);
+			System.out.println("Notas: ");
+			for (int j = 0; j < 4; j++) {
+				System.out.println("Nota: " + (j+1) + " - " + notas[i][j]);
+			}
+			System.out.println("Média: " + mediaAluno + " - " + statusAluno);
+		}
 	}
 
 }
--- JAVA/JavaIntermediario/Matrizes/src/module-info.java
+++ JAVA/JavaIntermediario/Matrizes/src/module-info.java
@@ -1,2 +1,3 @@
 module Matrizes {
+	requires java.desktop;
 }
