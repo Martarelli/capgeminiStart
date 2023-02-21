2eb5e12eb639539747f10e54c1fc128534e7beac
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-02-20 22:32:48 -0300

    add metodo enum

--- JAVA/JavaIntermediario/Construtores/src/Construtores/Aluno.java
+++ JAVA/JavaIntermediario/Construtores/src/Construtores/Aluno.java
@@ -5,11 +5,18 @@ public class Aluno {
 	private double nota2;
 	private double nota3;
 	STATUS situacaoAluno;
+	MATRICULA situacaoMatricula;
 	
 	public Aluno(double nota1, double nota2, double nota3) {
 		this.nota1 = nota1;
 		this.nota2 = nota2;
 		this.nota3 = nota3;
+	}
+	
+	public enum Matricula {
+		MATRICULADO,
+		PENDENTE,
+		RECUSADO
 	}
 	
 	public enum STATUS{
--- JAVA/JavaIntermediario/Construtores/src/Construtores/Matricula.java
+++
@@ -1,5 +0,0 @@
-package Construtores;
-
-public enum Matricula {
-
-}
