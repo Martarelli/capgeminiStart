e3106d5e6b5abf9973b67b1660c464dbeb524736
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-02-28 18:03:56 -0300

    exemplo sem generics

--- JAVA/JavaIntermediario/Generics/src/Controle/Aeronaves.java
+++ JAVA/JavaIntermediario/Generics/src/Controle/Aeronaves.java
@@ -1,5 +1,27 @@
 package Controle;
 
-public class Aeronaves {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Aeronaves {
+	private List<Integer> listaAeronaves = new ArrayList<Integer>();
+	
+	public void addVoo(Integer nrVoo) {
+		listaAeronaves.add(nrVoo);
+	}
+	
+	public Integer primeiroVoo() {
+		if(listaAeronaves.isEmpty()) {
+			throw new IllegalStateException("List is empty");
+		} else {
+			return listaAeronaves.get(0);
+		}
+	}
+	
+	public void listaVoos() {
+		for (Integer voo : listaAeronaves) {
+			System.out.println(voo);
+		}
+	}
+	
 }
--- JAVA/JavaIntermediario/Generics/src/Generics/Generics.java
+++ JAVA/JavaIntermediario/Generics/src/Generics/Generics.java
@@ -1,9 +1,31 @@
 package Generics;
 
+import java.util.Scanner;
+
+import Controle.Aeronaves;
+
 public class Generics {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//EXEMPLO SEM GENERICS
+		Scanner sc =  new Scanner(System.in);
+		
+		Aeronaves air = new Aeronaves();
+		System.out.println("numero voos:");
+		int nrAeronaves = sc.nextInt();
+		
+		for (int i = 0; i < nrAeronaves; i++) {
+			System.out.println("numero aeronave: ");
+			int nrVoo = sc.nextInt();
+			air.addVoo(nrVoo);
+		}
+		
+		System.out.println("Primeiro voo vai ser o nr: " + air.primeiroVoo());
+		air.listaVoos();
+		sc.close();
+		
+		
 
 	}
 
