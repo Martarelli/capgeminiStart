66f2be25159394304e8ba65080b2a5fab521decf
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-03-01 18:23:39 -0300

    uso pratico de threads

--- JAVA/JavaIntermediario/Threads/src/Threads/Threads.java
+++ JAVA/JavaIntermediario/Threads/src/Threads/Threads.java
@@ -1,10 +1,44 @@
 package Threads;
 
 public class Threads {
-
+	static int i = 0;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Thread(t1).start();
+		new Thread(t2).start();
 	}
+	
+	private static void contador(String name) {
+		i++;
+		System.out.println("O valor do contador atual é: " + i + " Thread: " + name);
+	}
+	
+	private static Runnable t1 = new Runnable() {
+		
+		@Override
+		public void run() {
+			try {
+				for(int i = 0 ; i < 20 ; i++) {
+					contador("t1");
+				}
+			} catch (Exception e) {
 
+			}
+			
+		}
+	};
+	
+	private static Runnable t2 = new Runnable() {
+		
+		@Override
+		public void run() {
+			try {
+				for(int i = 0 ; i < 20 ; i++) {
+					contador("t2");
+				}
+			} catch (Exception e) {
+
+			}
+			
+		}
+	};
 }
