8f59be856a1fde0d1f04e62f833ef81eeae92e48
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-03-02 18:27:02 -0300

    exemplo sem for each

--- JAVA/JavaIntermediario/Loops/src/Loops/Loops.java
+++ JAVA/JavaIntermediario/Loops/src/Loops/Loops.java
@@ -4,7 +4,12 @@ public class Loops {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] vetorNumeros = new int[] {1,2,3,4,5,6,7,8,9,10};
+		
+		System.out.println("Sem for each");
+		for (int i = 0; i < vetorNumeros.length; i++) {
+			System.out.println("Nr: "+ vetorNumeros[i]);
+		}
 	}
 
 }
