079635e37f573d302d0bae47055aba10bd7946e9
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-03-03 19:42:59 -0300

    pratica de uso de dates

--- JAVA/JavaIntermediario/Dates/src/Dates/Dates.java
+++ JAVA/JavaIntermediario/Dates/src/Dates/Dates.java
@@ -1,10 +1,43 @@
 package Dates;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class Dates {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		SimpleDateFormat sdf3 = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		sdf3.setTimeZone(TimeZone.getTimeZone("GMT"));
+		Date dt1 = new Date();
+		Date dt2 = new Date(System.currentTimeMillis());
+		Date dt3 = new Date(0L);
+		Date dt4 = new Date(1000L * 60L * 60L * 5L);
 
+		System.out.println("dt1: "+ dt1);
+		System.out.println("dt2: "+ dt2);
+		System.out.println("dt3: "+ dt3);
+		System.out.println("dt4: "+ dt4);
+
+		System.out.println("-------------------");
+		System.out.println("dt1: "+ sdf1.format(dt1));
+		System.out.println("dt2: "+ sdf1.format(dt2));
+		System.out.println("dt3: "+ sdf1.format(dt3));
+		System.out.println("dt4: "+ sdf1.format(dt4));
+
+		System.out.println("-------------------");
+		System.out.println("dt1: "+ sdf2.format(dt1));
+		System.out.println("dt2: "+ sdf2.format(dt2));
+		System.out.println("dt3: "+ sdf2.format(dt3));
+		System.out.println("dt4: "+ sdf2.format(dt4));
+		
+		System.out.println("-------------------");
+		System.out.println("dt1: "+ sdf3.format(dt1));
+		System.out.println("dt2: "+ sdf3.format(dt2));
+		System.out.println("dt3: "+ sdf3.format(dt3));
+		System.out.println("dt4: "+ sdf3.format(dt4));
 	}
 
 }
