d1b83927f6f57fc54317449aa4267b07406fee46
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-03-07 17:46:26 -0300

    preparação da classe dao

--- JAVA/JavaIntermediario/PersistenciaBD/src/Cadastros/Dao.java
+++ JAVA/JavaIntermediario/PersistenciaBD/src/Cadastros/Dao.java
@@ -1,5 +1,17 @@
 package Cadastros;
 
-public class Dao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import com.mysql.cj.jdbc.CallableStatement;
+
+public class Dao {
+	
+	Connection con; //Conexão ao BD
+	PreparedStatement stmt; // Acessa Tabela (insert, update, delete, select)
+	ResultSet rs; //Consulta Tabela
+	CallableStatement call; //Procedures e Function
+	
+	
 }
