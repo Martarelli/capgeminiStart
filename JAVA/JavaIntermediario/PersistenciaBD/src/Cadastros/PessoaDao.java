66a0337441ee02298d5d4aea6cd54abc663d935b
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-03-08 18:13:24 -0300

    metodo listar pessoa

--- JAVA/JavaIntermediario/PersistenciaBD/src/Cadastros/PessoaDao.java
+++ JAVA/JavaIntermediario/PersistenciaBD/src/Cadastros/PessoaDao.java
@@ -1,8 +1,11 @@
 package Cadastros;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PessoaDao extends Dao{
 	
-	public void incluirPessoa(Pessoa p) throws Exception{
+	public void IncluirPessoa(Pessoa p) throws Exception{
 		open();
 		stmt = con.prepareStatement("insert into pessoa values(?,?,?)");
 		stmt.setInt(1, p.getIdPessoa());
@@ -13,7 +16,7 @@ public class PessoaDao extends Dao{
 		close();
 	}
 	
-	public void alterarPessoa(Pessoa p) throws Exception{
+	public void AlterarPessoa(Pessoa p) throws Exception{
 		open();
 		stmt = con.prepareStatement("update pessoa nome=?,email=? where idPessoa= ?");
 		stmt.setInt(1, p.getIdPessoa());
@@ -24,12 +27,45 @@ public class PessoaDao extends Dao{
 		close();
 	}
 	
-	public void excluirPessoa(Pessoa p) throws Exception{
+	public void ExcluirPessoa(Pessoa p) throws Exception{
 		open();
 		stmt = con.prepareStatement("delete from pessoa where idPessoa = ?");
 		stmt.setInt(1, p.getIdPessoa());
 		stmt.execute();
 		stmt.close();
 		close();
+	}
+	
+	public Pessoa ConsultarPessoa(int cod) throws Exception{
+		open();
+		stmt = con.prepareStatement("select * from pessoa where idPessoa = ?");
+		rs = stmt.executeQuery();
+		Pessoa p = null;
+		if(rs.next()) {
+			p = new Pessoa();
+			p.setIdPessoa(rs.getInt("idPessoa"));
+			p.setNomePessoa(rs.getString("nomePessoa"));
+			p.setEmail(rs.getString("email"));
+		}
+		close();
+		return p;
+	}
+	
+	public List<Pessoa> ListarPessoa() throws Exception{
+		
+		
+		open();
+		stmt = con.prepareStatement("select * from pessoa");
+		rs = stmt.executeQuery();
+		List<Pessoa> listaPessoas = new ArrayList<Pessoa>()
+		while(rs.next()) {
+			Pessoa p = new Pessoa();
+			p.setIdPessoa(rs.getInt("idPessoa"));
+			p.setNomePessoa(rs.getString("nomePessoa"));
+			p.setEmail(rs.getString("email"));
+			listaPessoas.add(p);
+		}
+		close();
+		return listaPessoas;
 	}
 }
