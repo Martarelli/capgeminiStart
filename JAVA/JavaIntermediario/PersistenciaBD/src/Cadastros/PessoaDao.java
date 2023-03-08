02f142a40c625d12ce34ca18755facef130851d0
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-03-08 17:57:35 -0300

    metodo incluir pessoa

--- JAVA/JavaIntermediario/PersistenciaBD/src/Cadastros/PessoaDao.java
+++ JAVA/JavaIntermediario/PersistenciaBD/src/Cadastros/PessoaDao.java
@@ -1,5 +1,15 @@
 package Cadastros;
 
-public class PessoaDao {
-
+public class PessoaDao extends Dao{
+	
+	public void incluirPessoa(Pessoa p) throws Exception{
+		open();
+		stmt = con.prepareStatement("insert into pessoa values(?,?,?)");
+		stmt.setInt(1, p.getIdPessoa());
+		stmt.setString(2, p.getNomePessoa());
+		stmt.setString(3, p.getEmail());
+		stmt.execute();
+		stmt.close();
+		close();
+	}
 }
