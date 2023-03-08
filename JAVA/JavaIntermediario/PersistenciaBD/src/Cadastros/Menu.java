9b7d2fd7aec9a65d6f46757798fe1d58199adaf3
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-03-08 18:18:56 -0300

    inserção de pessoas no bd

--- JAVA/JavaIntermediario/PersistenciaBD/src/Cadastros/Menu.java
+++ JAVA/JavaIntermediario/PersistenciaBD/src/Cadastros/Menu.java
@@ -3,7 +3,15 @@ package Cadastros;
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		PessoaDao pd = new PessoaDao();
+		try {
+			Pessoa p1 = new Pessoa(1,"Martarelli","email@email.com");
+			Pessoa p2 = new Pessoa(2,"Renan","exemplo@email.com");
+			pd.IncluirPessoa(p1);
+			pd.IncluirPessoa(p2);
+		} catch(Exception e) {
+			System.out.println(e);
+		}
 
 	}
 
